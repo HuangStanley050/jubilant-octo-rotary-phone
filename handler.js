83b40ea6bd2b3c8ea500b4b74925d636672c681e
Stanley Huang <huang.stanley050@gmail.com>
2020-03-11 20:13:56 +1100

    removed some option in return statement

--- handler.js
+++ handler.js
@@ -3,14 +3,10 @@
 module.exports.hello = async event => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Testing github actions",
-        input: event
-      },
-      null,
-      2
-    )
+    body: JSON.stringify({
+      message: "Testing github actions",
+      input: event
+    })
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
